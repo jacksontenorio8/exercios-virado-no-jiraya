6a91cdcc43b5df79eef4173d7b499f56f2a95990
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-02-04 20:58:57 -0300

    Criei a classe Exercicio3.java
    Programa que lê uma temperatura em graus celcius e converte para fahrenhit.

---
+++ src/br/com/jacksontenorio8/Modulo1/Exercicio3.java
@@ -0,0 +1,17 @@
+package br.com.jacksontenorio8.Modulo1;
+import java.io.IOException;
+import java.util.Scanner;
+
+//Programa que realize a conversão de graus Celsius (C) para graus Fahrenheit (F).
+
+public class Exercicio3 {
+    public static void main(String[] args) throws IOException {
+
+        Scanner teclado = new Scanner(System.in);
+        
+        double C = teclado.nextDouble();
+        double F = (9 * C) / 5;
+
+        System.out.println(String.format("Temperatua em grau Fahrenhit: %.1f", F));
+    }
+}
