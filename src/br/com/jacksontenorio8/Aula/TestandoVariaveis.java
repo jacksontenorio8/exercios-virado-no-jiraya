48b313bad27be1f2443c099a5cd7d4ee07d3d937
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-02-18 20:21:34 -0300

    Exercício virado no jiraya sobre as vaiáveis: String, char e boolean.

---
+++ src/br/com/jacksontenorio8/Aula/TestandoVariaveis.java
@@ -0,0 +1,15 @@
+package br.com.jacksontenorio8.Aula;
+
+public class TestandoVariaveis {
+    public static void main(String[] args) {
+        char masculino = 'M';
+        char feminino = 'F';
+        String nome = "Jackson Tenorio da Silva";
+        boolean condicao = false;
+
+        System.out.println(masculino);
+        System.out.println(feminino);
+        System.out.println(nome);
+        System.out.println(condicao);
+    }
+}
