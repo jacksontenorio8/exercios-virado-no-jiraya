7d02bebc2db9ac3c725babed0ab113f78a603441
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-03-04 17:14:52 -0300

    Controle de fluxo if-else parte 02.

---
+++ src/br/com/jacksontenorio8/Aula/VerificarIdade.java
@@ -0,0 +1,16 @@
+package br.com.jacksontenorio8.Aula;
+
+public class VerificarIdade {
+    public static void main(String[] args) {
+        int idade;
+        boolean teste = false;
+
+        idade = 34;
+
+        if (teste = true){
+            System.out.println("Adulto");
+        } else{
+            System.out.println("Nao Adulto");
+        }
+    }
+}
