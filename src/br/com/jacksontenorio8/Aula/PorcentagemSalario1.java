f65ca4a8b0eda80af28984c2e2dc50021c4dbbad
jacksontenorio8 <jacksontenorio8@gmail.com>
2022-03-18 16:09:37 -0300

    Update

--- src/br/com/jacksontenorio8/Aula/PorcentagemSalario1.java
+++ src/br/com/jacksontenorio8/Aula/PorcentagemSalario1.java
@@ -1,17 +1,16 @@
 package br.com.jacksontenorio8.Aula;
-import java.util.Scanner;
 
 public class PorcentagemSalario1 {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
 
-        double salario = teclado.nextDouble();
+
+        double salario = 4000.50;
         double novoSalario1 = 4500 + (4500 * 0.3);
         double novoSalario2 = 4500 + (4500 * 0.15);
 
-        if(salario > 4500) {
+        if (salario > 4500) {
             System.out.println(novoSalario1);
-        }else{
+        } else {
             System.out.println(novoSalario2);
         }
     }
